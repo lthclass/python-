c283cb0b5cace00b84cf4064379151ce4d43ea25
lthclass <101073281+lthclass@users.noreply.github.com>
2022-03-21 10:51:02 +0900

    Create 3주차 20220307.py

---
+++ 3주차 20220307.py
@@ -0,0 +1,86 @@
+'''
+2022_03_14
+3주차 수업
+'''
+
+a = "200"
+
+#a가 가리키는 자료의 종류를 출력
+print (type(a))
+
+'''
+#a가 가리키는 자료를 정수로 변환후
+#a가 그 값을 가리킴
+a = int(a)
+
+
+#문자열 "3.14"를 실수숫자로 바꾼 후
+# b가 가리키도록 하는 코드 작성
+b= float("3.14");
+print( type(b))
+
+
+#정수 3을 시수로 바꾸면 어떻게 될 까요?
+c = float(3)
+print(c) #정수 3은 실수 3.0으로 바뀝니다.
+'''
+
+'''
+BMI (body measure index) /체질량 지수
+bmi는 몸무게(kg)를 키(미터)의 제곱값으로 나눈 숫자
+
+'''
+#문제
+'''
+키와 몸무게를 입력으로 받아,
+bmi값을 출력해주는 프로그램을 작성해 봅시다.
+'''
+'''
+cm = float(input("키를 입력하시오(cm): "))    
+kg = float(input("몸무게를 입력하시오(kg): "))    
+
+cm = float(cm /100)
+print(kg / (cm * cm))
+'''
+'''
+a = float(input("키를 입력하세요(cm);"))
+b = float(input("몸무게를 입력하세요(kg);"))
+
+a = float( a / 100)
+print( b / (a*a))
+
+'''
+
+
+
+
+'''
+a = float(input("키(cm);"))
+b = float(input("몸무게(kg);"))
+
+a= float( a /100)
+print(b / (a*a))
+'''
+
+
+
+
+'''
+a = float(input("키(cm);"))
+b = float(input("몸무게(kg);"))
+
+
+a = float(a/100)
+print( b/ (a*a))
+
+
+'''
+
+
+
+a=float(input("키(cm);"))
+b=float(input("몸무게(kg);"))
+
+a = float( a/100)
+print(b /(a*a))
+
